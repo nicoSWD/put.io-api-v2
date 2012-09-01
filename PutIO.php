5a60545e0240096718ea6ee0ea6491feb4989482
Nico <nico@nicoswd.com>
2012-09-01 14:24:52 +0200

    Updated note about the Wiki

--- PutIO.php
+++ PutIO.php
@@ -7,7 +7,8 @@
  * @license GPL 3 http://www.gnu.org/copyleft/gpl.html
  *
  * This class enabled easy access to put.io's API (version 2)
- * See the /Examples folder for detailed instructions.
+ * Take a look at the Wiki for detailed instructions:
+ * https://github.com/nicoSWD/put.io-API-v2-Wrapper/wiki
  *
  * @license
  * This program is free software: you can redistribute it and/or modify
