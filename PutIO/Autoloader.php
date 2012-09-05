2a5a47ae55f8975f11d49c6ab7dfc1293f2a2e6e
Nico Oelgart <nico@nicoswd.com>
2012-09-05 16:38:50 +0300

    Update PutIO/Autoloader.php

--- PutIO/Autoloader.php
+++ PutIO/Autoloader.php
@@ -13,22 +13,19 @@
 
 spl_autoload_register(function($className)
 {
-    if (strpos($className, 'PutIO') === 0)
-    {
-        $className = ltrim($className, '\\');
-        $fileName  = '';
-        $namespace = '';
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
         
-        if ($lastNsPos = strripos($className, '\\'))
-        {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-        }
-       
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-        require $fileName;
+    if ($lastNsPos = strripos($className, '\\'))
+    {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
+       
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    require $fileName;   
 });
 
 ?>
