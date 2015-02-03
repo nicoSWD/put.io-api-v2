a8b359755806f39d4c8df6573c59598c9f57208d
Nico <nico@Nicos-Mac-Pro.local>
2015-02-03 22:56:38 +0100

    PSR-2 + cleanup

---
+++ src/Engines/PutIO/FriendsEngine.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Copyright (C) 2012  Nicolas Oelgart
+ *
+ * @author Nicolas Oelgart
+ * @license GPL 3 http://www.gnu.org/copyleft/gpl.html
+ *
+ * This class manages anything related to 'Friends'.
+ * For precise return values see put.io's documentation here:
+ *
+ * https://api.put.io/v2/docs/#friends
+ */
+namespace PutIO\Engines\PutIO;
+
+use PutIO\Helpers\PutIO\PutIOHelper;
+
+final class FriendsEngine extends PutIOHelper
+{
+    /**
+     * Returns an array of all friends.
+     *
+     * @return array 
+     */
+    public function listall()
+    {
+        return $this->get('friends/list', [], \false, 'friends');
+    }
+    
+    /**
+     * Returns an array of pending requests.
+     *
+     * @return array
+     */
+    public function pendingRequests()
+    {
+        return $this->get('friends/waiting-requests', [], \false, 'friends');
+    }
+    
+    /**
+     * Sends out a friend request to a specific user.
+     *
+     * @param string $username User to receive friend request
+     * @return boolean
+     */
+    public function sendRequest($username)
+    {
+        return $this->post('friends/' . $username . '/request', [], \true);
+    }
+    
+    /**
+     * Denies a specific friend request.
+     *
+     * @param string $username User to have their request denied
+     * @return boolean
+     *
+     */
+    public function denyRequest($username)
+    {
+        return $this->post('friends/' . $username . '/deny', [], \true);
+    }
+}
